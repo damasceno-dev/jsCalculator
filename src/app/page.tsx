345a0a46de300030c2c29207b000d619cd967092
Fast Ninja <damasceno.developer21@gmail.com>
2023-07-25 11:40:30 -0300

    first commit: project structure with initial design and logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,94 @@
-import Image from 'next/image'
+'use client'
+import React, {MouseEvent, useState } from "react"
+
 
 export default function Home() {
+  const Operations = ['+', '-', '/', '*']
+  const [expression, setExpression] = useState('0');
+
+  var splitOp = /(\*|\+|\-|\/)/
+  const numbersArray = expression.split(splitOp);
+  const lastValue = numbersArray[numbersArray.length -1]
+  console.log(numbersArray)
+  console.log(lastValue)
+  function handleExpression(event : MouseEvent<HTMLElement>) {
+    setExpression(prev => prev + (event.target as Element).id)
+  }
+
+  function handleClear() {
+    setExpression('0')
+  }
+
+  function handleOperation(event : MouseEvent<HTMLElement>) {
+    
+    let lastCharacter = expression.charAt(expression.length -1);
+
+    if (Operations.includes(lastCharacter)) {
+      let newExpression = expression.slice(0,-1);
+      setExpression(newExpression + (event.target as Element).id);
+    } else {
+      setExpression(prev => prev + (event.target as Element).id)
+    }
+  }
+
+  function handleDecimalPoint(event : MouseEvent<HTMLElement>) {
+    if (lastValue === '') {
+      let valueToAdd = '0.'
+      setExpression(prev => prev + valueToAdd);
+      return;
+    }
+    console.log(lastValue.includes('.'))
+    if (lastValue.includes('.')) {
+      return;
+    } else {
+      setExpression(prev => prev + (event.target as Element).id)
+    }
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex items-center justify-center h-screen bg-slate-800 text-white select-none">
+      <div className='w-80 bg-slate-500 border border-white'>
+        <div className='min-h-[2.5rem] break-all w-full bg-slate-600 flex place-content-end place-items-center pr-1 text-2xl'>{expression}</div>
+        <div className='h-7 w-full bg-slate-700'></div>
+        <div id="grid-container" className="grid grid-cols-4 gap-[1px]">
+
+          <button className="bg-black p-5 col-span-2" onClick={handleClear} id='AC'> AC</button>
+          <button className="bg-black p-5" onClick={handleOperation} id='/'>/</button>
+          <button className="bg-black p-5" onClick={handleOperation} id='*'>*</button>
+
+          <button className="bg-black p-5" onClick={handleExpression} id='7'>7</button>
+          <button className="bg-black p-5" onClick={handleExpression} id='8'>8</button>
+          <button className="bg-black p-5" onClick={handleExpression} id='9'>9</button>
+          <button className="bg-black p-5" onClick={handleOperation} id='-'>-</button>
+
+          <button className="bg-black p-5" onClick={handleExpression} id='4'>4</button>
+          <button className="bg-black p-5" onClick={handleExpression} id='5'>5</button>
+          <button className="bg-black p-5" onClick={handleExpression} id='6'>6</button>
+          <button className="bg-black p-5" onClick={handleOperation} id='+'>+</button>
+
+          <button className="bg-black p-5" onClick={handleExpression} id='1'>1</button>
+          <button className="bg-black p-5" onClick={handleExpression} id='2'>2</button>
+          <button className="bg-black p-5" onClick={handleExpression} id='3'>3</button>
+          <button className="bg-black p-5 row-span-2" onClick={handleOperation} id='='>=</button>
+
+          <button className="bg-black p-5 col-span-2" onClick={handleExpression} id='0'>0</button>
+          <button className="bg-black p-5" onClick={handleDecimalPoint} id='.'>.</button>
+          
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+  )
+
+}
+
+interface ButtonProps {
+  style: string;
+}
+
+function Button({style} : ButtonProps) {
+  let styleToReturn = 'p-5 ' + style;
+  return (
+    <button></button>
   )
 }
---
+++ to-do.md
@@ -0,0 +1,8 @@
+# todo:
+> handle left zero
+> font for buttons
+> font for display (calculator font)
+> make one button that, when clicked, his number goes to display
+> button design (using react.dev pallete)
+> display design (using fcc example pallete)
+> github repository and push
