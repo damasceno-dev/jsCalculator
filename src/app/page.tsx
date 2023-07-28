2a7b8c4db307a5b45ffff51be7be37bba8099711
Fast Ninja <damasceno.developer21@gmail.com>
2023-07-28 11:00:22 -0300

    extracted calc items array to avoid repetition when rendering all buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,65 @@
 'use client'
 import React, {MouseEvent, useState } from "react"
 
+interface calcItem {
+   id: string;
+   operation: string;
+   execution: (event: MouseEvent<HTMLElement>) => void;
+   style?: string;
+}
+
+type calcArray = calcItem[]
 
 export default function Home() {
+     
+  const calcItems : calcArray = [
+    {id: 'op-clear', operation: 'AC', execution: handleClear},
+    {id: 'op-division', operation: '/', execution: handleOperation},
+    {id: 'op-multiplication', operation: '*', execution: handleOperation},
+    {id: 'delete', operation: '<', execution: handleDeletion},
+
+    {id: 'number-seven', operation: '7', execution: handleExpression},
+    {id: 'number-eigth', operation: '8', execution: handleExpression},
+    {id: 'number-nine', operation: '9', execution: handleExpression},
+    {id: 'op-substraction', operation: '-', execution: handleOperation},
+
+    {id: 'number-four', operation: '4', execution: handleExpression},
+    {id: 'number-five', operation: '5', execution: handleExpression},
+    {id: 'number-six', operation: '6', execution: handleExpression},
+    {id: 'op-sum', operation: '+', execution: handleOperation},
+
+    {id: 'number-one', operation: '1', execution: handleExpression},
+    {id: 'number-two', operation: '2', execution: handleExpression},
+    {id: 'number-three', operation: '3', execution: handleExpression},
+    {id: 'op-result', operation: '=', execution: handleCalculation, style: 'row-span-2'},
+
+    {id: 'number-zero', operation: '0', execution: handleExpression, style: 'col-span-2'},
+    {id: 'decimal-point', operation: '.', execution: handleDecimalPoint}]
+
+
   const Operations = ['+', '-', '/', '*']
   const [expression, setExpression] = useState('0');
 
-  var splitOp = /(\*|\+|\-|\/)/
+  const splitOp = /(\*|\+|\-|\/)/;
   const numbersArray = expression.split(splitOp);
-  const lastValue = numbersArray[numbersArray.length -1]
+  const lastElement = numbersArray[numbersArray.length -1];
+  const lastValue = lastElement === '' ? numbersArray[numbersArray.length -2] : lastElement;
   console.log(numbersArray)
+  console.log(lastElement)
   console.log(lastValue)
+  
   function handleExpression(event : MouseEvent<HTMLElement>) {
-    setExpression(prev => prev + (event.target as Element).id)
+
+    let nextValue = (event.target as Element).id;
+
+    if (lastValue === '0' && nextValue === '0') {
+      return;
+    } else if (lastValue === '0' && nextValue !== '0') {
+      let newExpression = expression.slice(0,-1);
+      setExpression(newExpression + nextValue);
+    } else {
+      setExpression(prev => prev + nextValue)
+    }
   }
 
   function handleClear() {
@@ -20,7 +67,7 @@ export default function Home() {
   }
 
   function handleOperation(event : MouseEvent<HTMLElement>) {
-    
+
     let lastCharacter = expression.charAt(expression.length -1);
 
     if (Operations.includes(lastCharacter)) {
@@ -37,7 +84,6 @@ export default function Home() {
       setExpression(prev => prev + valueToAdd);
       return;
     }
-    console.log(lastValue.includes('.'))
     if (lastValue.includes('.')) {
       return;
     } else {
@@ -45,14 +91,37 @@ export default function Home() {
     }
   }
 
+  function handleDeletion() {
+    let newExpression = expression.slice(0,-1);
+    if (newExpression === '') {
+      newExpression = '0';
+    }
+    setExpression(newExpression)
+  }
+
+  function handleCalculation() {
+    // if lastvalue != number { pop last value }
+    // while array.length > 1 {
+    //   handlemultiplyAndDivision: go throught array searching / or *
+    //   if it finds it, replace n-1 and n+1 for the result of this calculation 
+    //   handleSumAndSubstract: same as above, but for + or -
+    // }
+    let result = numbersArray;
+    if (parseInt(lastValue) == 'NaN') {
+      result.pop();
+    }
+    console.log(result)
+
+
+  }
 
   return (
     <main className="flex items-center justify-center h-screen bg-slate-800 text-white select-none">
       <div className='w-80 bg-slate-500 border border-white'>
-        <div className='min-h-[2.5rem] break-all w-full bg-slate-600 flex place-content-end place-items-center pr-1 text-2xl'>{expression}</div>
-        <div className='h-7 w-full bg-slate-700'></div>
+        <div className='min-h-[2.5rem] break-all text-right w-full bg-slate-600 flex place-content-end place-items-center pr-1 text-2xl'>{expression}</div>
+        <div className='h-7 w-full text-right bg-slate-700 pr-1'>{lastValue}</div>
         <div id="grid-container" className="grid grid-cols-4 gap-[1px]">
-
+{/* 
           <button className="bg-black p-5 col-span-2" onClick={handleClear} id='AC'> AC</button>
           <button className="bg-black p-5" onClick={handleOperation} id='/'>/</button>
           <button className="bg-black p-5" onClick={handleOperation} id='*'>*</button>
@@ -73,7 +142,36 @@ export default function Home() {
           <button className="bg-black p-5 row-span-2" onClick={handleOperation} id='='>=</button>
 
           <button className="bg-black p-5 col-span-2" onClick={handleExpression} id='0'>0</button>
-          <button className="bg-black p-5" onClick={handleDecimalPoint} id='.'>.</button>
+          <button className="bg-black p-5" onClick={handleDecimalPoint} id='.'>.</button> */}
+
+
+
+          {calcItems.map(calculatorItem => <CalcButton key={calculatorItem.id} {...calculatorItem}>
+                                            {calculatorItem.operation}
+                                           </CalcButton>)}
+
+          {/* <CalcButton execution={handleClear}> AC</CalcButton>
+          <CalcButton execution={handleOperation}>/</CalcButton>
+          <CalcButton execution={handleOperation}>*</CalcButton>
+          <CalcButton execution={handleDeletion}>&lt;</CalcButton>
+
+          <CalcButton execution={handleExpression}>7</CalcButton>
+          <CalcButton execution={handleExpression}>8</CalcButton>
+          <CalcButton execution={handleExpression}>9</CalcButton>
+          <CalcButton execution={handleOperation}>-</CalcButton>
+
+          <CalcButton execution={handleExpression}>4</CalcButton>
+          <CalcButton execution={handleExpression}>5</CalcButton>
+          <CalcButton execution={handleExpression}>6</CalcButton>
+          <CalcButton execution={handleOperation}>+</CalcButton>
+
+          <CalcButton execution={handleExpression}>1</CalcButton>
+          <CalcButton execution={handleExpression}>2</CalcButton>
+          <CalcButton execution={handleExpression}>3</CalcButton>
+          <CalcButton execution={handleCalculation} style=" row-span-2" >=</CalcButton>
+
+          <CalcButton execution={handleExpression} style=" col-span-2" >0</CalcButton>
+          <CalcButton execution={handleDecimalPoint}>.</CalcButton> */}
           
         </div>
       </div>
@@ -83,12 +181,18 @@ export default function Home() {
 }
 
 interface ButtonProps {
-  style: string;
+  style?: string;
+  background?: string;
+  children: React.ReactNode;
+  execution: (event : MouseEvent<HTMLElement>) => void;
 }
 
-function Button({style} : ButtonProps) {
-  let styleToReturn = 'p-5 ' + style;
+function CalcButton({style, background = 'bg-black', children, execution} : ButtonProps) {
+  const styleToReturn = 'p-5 ' + style + ' ' + background;
+  const textRender = children?.toString();
   return (
-    <button></button>
+    <button className={styleToReturn} onClick={execution} id={textRender}>
+      {children}
+    </button>
   )
 }
--- to-do.md
+++ to-do.md
@@ -1,8 +1,12 @@
 # todo:
-> handle left zero
+> verify tests
+> (ok) handle left zero
+> (ok) button recieves children as props, which is going to be a text, and its children is its id
+> button glow blue borders when hovered
+> button for rollback one
 > font for buttons
 > font for display (calculator font)
-> make one button that, when clicked, his number goes to display
-> button design (using react.dev pallete)
-> display design (using fcc example pallete)
-> github repository and push
+> (ok) make one button that, when clicked, his number goes to display
+> (ok) button design (using react.dev pallete)
+> (ok) display design (using fcc example pallete)
+> (ok) github repository and push
